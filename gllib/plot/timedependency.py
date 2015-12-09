27b78608b190ce43ef166f22b6f2774b14a9c8e5
Nicolas 'keksnicoh' Heimann <keksnicoh@heimann.hamburg>
2015-12-09 18:09:02 +0100

    SOME IUPDAZE

--- gllib/plot/timedependency.py
+++ gllib/plot/timedependency.py
@@ -5,6 +5,8 @@ time dependency utilities
 """
 from time import time 
 
+from gllib.util import Event 
+
 class PlotTicker():
     """
     simple object which can be attached to
@@ -12,10 +14,14 @@ class PlotTicker():
     the plotter to replot when time 
     changes.
     """
-    def __init__(self):
+    def __init__(self, tick_event=None):
+        self.on_tick = Event()
+        if tick_event is not None:
+            self.on_tick.append(tick_event)
         self.start_time = time();
     def __call__(self, plotter):
         time_now = time() - self.start_time
+        self.on_tick(time_now)
         for graph in plotter.graphs.values():
             if hasattr(graph, 'set_time'):
                 graph.set_time(time_now)

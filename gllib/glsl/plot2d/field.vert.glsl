b9322ebea56494bae4113d020250aa23989ba93c
Nicolas 'keksnicoh' Heimann <keksnicoh@heimann.hamburg>
2015-12-15 16:52:03 +0100

    field plot fix

--- gllib/glsl/plot2d/field.vert.glsl
+++ gllib/glsl/plot2d/field.vert.glsl
@@ -4,6 +4,8 @@ in vec2 vertex_position;
 in vec2 texture_position;
 out vec2 fragment_position;
 uniform mat3 mat_domain;
+uniform mat4 mat_outer_camera;
+
 uniform mat4 mat_camera;
 uniform float zoom;
 void main() {

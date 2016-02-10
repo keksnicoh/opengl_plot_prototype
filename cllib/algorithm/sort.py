47caed993abe28937f49b3bd0789f3978fc01e70
Ginni <ginni@dhcp-172-21-13-87.wlan.uni-hamburg.de.local>
2016-02-10 17:57:50 +0100

    sorting

---
+++ cllib/algorithm/sort.py
@@ -0,0 +1,184 @@
+import pystache
+import pyopencl as cl
+import numpy as np
+import pyopencl.array
+from cllib.common import kernel_helpers
+
+class Related():
+	"""
+	sort multiple systems of simple array and applies order to additional buffers
+	@todo:
+		- define sorting order (descending ascending)
+		- key-function for more specific key-arrays (with different types)
+		- extract bubble sort algorithm to exchange with different sorting techniques
+		- fix issue with while-loop condition if sort is done earlier (set dim to 10)
+	"""
+	SWAP_SRC = """
+		for(int l=0; l < {{{BLOCKSIZE}}}; l++) {
+			{{{DTYPE}}} b = {{{BUFFER_NAME}}}[glid*{{{DIMENSION}}}*{{{BLOCKSIZE}}} + i*{{{DIMENSION}}} + l];
+			{{{BUFFER_NAME}}}[glid*{{{DIMENSION}}}*{{{BLOCKSIZE}}} + i*{{{DIMENSION}}} + l] = {{{BUFFER_NAME}}}[glid*{{{DIMENSION}}}*{{{BLOCKSIZE}}} + (i+1)*{{{DIMENSION}}} + l];
+			{{{BUFFER_NAME}}}[glid*{{{DIMENSION}}}*{{{BLOCKSIZE}}} + (i+1)*{{{DIMENSION}}} + l] = b;
+		}
+	"""
+
+	SORT_KERNEL = """
+		{{{LIBS}}}
+		{{{STRUCTS}}}
+	__kernel void bubble_sort(__global float* values, {{{RELATED_BUFFERS}}}) 
+		{
+			int glid = get_global_id(0);
+
+			__private bool did_change_something = true;
+
+			int c = 0;
+			while(did_change_something) {
+				//did_change_something = false;
+				for(int i=0; i < {{{DIMENSION}}}-1; i++) {
+					if (values[glid*{{{DIMENSION}}}+ i] {{{ORDER}}} values[glid*{{{DIMENSION}}}+ i+1]) {
+						did_change_something = true;
+						float bigger = values[glid*{{{DIMENSION}}}+ i];
+
+						values[glid*{{{DIMENSION}}}+ i]   = values[glid*{{{DIMENSION}}}+ i+1];
+						values[glid*{{{DIMENSION}}}+ i+1] = bigger;
+
+						{{{SHIFT_RELATED_BUFFERS}}}
+					}
+				}
+
+				c++;
+				if (c>{{{DIMENSION}}}) {
+					did_change_something = false;
+				}
+			}	
+		}
+	"""
+
+	def __init__(self, ctx, key_shape, blocksizes=[]):
+		self.ctx = ctx
+		self._key_shape = key_shape
+
+		is_tuple  = lambda x: type(x) is tuple 
+		only_type = lambda x: len(x) == 1
+		get_bs    = lambda x: x[1] if is_tuple(x) and not only_type(x) else 1
+		get_dtype = lambda x: x[0] if is_tuple(x) else x
+
+		self._blocksizes = [(get_dtype(x), get_bs(x)) for x in blocksizes]
+
+	def _build_related_buffer_block(self, buffer_id, bs):
+		return pystache.render(self.SWAP_SRC, {
+			'BUFFER_NAME': 'r_{}'.format(buffer_id),
+			'BLOCKSIZE': str(bs[1]),
+			'DTYPE': bs[0],
+			'DIMENSION': self._key_shape[1]
+		}).encode('ascii')
+
+	def build(self, descending=False):
+		arguments = [('r_{}'.format(i), 'global', x[0], '*r_{}'.format(i)) for i, x in enumerate(self._blocksizes)]
+		arguments, strcts, cl_arg_declr, libs = kernel_helpers.process_arguments_declaration(self.ctx.devices[0], arguments)
+
+		related_buffer_blocks = [self._build_related_buffer_block(i, (arguments[i][2], x[1]))  for i, x in enumerate(self._blocksizes)]
+
+		src = pystache.render(self.SORT_KERNEL, {
+			'LIBS': libs,
+			'STRUCTS': '\n'.join(strcts), 
+		    'RELATED_BUFFERS': ', '.join(cl_arg_declr),
+		    'SHIFT_RELATED_BUFFERS': '\n'.join(related_buffer_blocks),
+		    'ORDER': '<' if descending else '>',
+		    'DIMENSION': self._key_shape[1]
+		}).encode('ascii')
+
+		self._program = cl.Program(self.ctx, src).build()
+
+	def __call__(self, queue, values_buffer, *args):
+		self._program.bubble_sort(queue, (self._key_shape[0],), None, values_buffer, *args)
+
+
+if __name__ == '__main__':
+	dim     = 10
+	systems = 3
+	descending = False
+
+	random_search_array = np.array(np.random.rand(dim*systems), dtype=np.float32).reshape(systems, dim)
+
+	random_related_arrays = [
+		np.array(range(systems*dim*dim), dtype=np.float32).reshape(systems, dim, dim)
+	]
+
+	platform = cl.get_platforms()[0]
+	devices = platform.get_devices()
+	gpu_device = devices[1]
+	ctx    = cl.Context(devices=[gpu_device])
+	queue = cl.CommandQueue(ctx)
+	
+	sort = Related(ctx, random_search_array.shape, [(np.float32, x.shape[1]) for x in random_related_arrays])
+
+	if descending:
+		sort.build(descending=True)
+
+		cl_search_array = cl.array.to_device(queue, random_search_array)
+		cl_related_arrays = [cl.array.to_device(queue, x) for x in random_related_arrays]
+		sort(queue, cl_search_array.data, *[x.data for x in cl_related_arrays])
+
+		cl_sorted = cl_search_array.get()
+		for system in range(systems):
+			assert np.array_equal(cl_sorted[system], np.sort(random_search_array[system])[::-1])
+
+		index_of_sort = np.argsort(random_search_array)[:,::-1]
+
+		sorted_related_arrays = []
+		for related_array in random_related_arrays:
+			sorted_related_array = np.zeros(systems*dim*dim).reshape(systems, dim, dim)
+			for system in range(systems):
+				sorted_related_array[system] = related_array[system][index_of_sort[system]]
+
+			sorted_related_arrays.append(sorted_related_array)
+
+			for cl_data, np_data in zip(cl_related_arrays, sorted_related_arrays):
+				#print(cl_data.get())
+				#print(np_data)
+				assert np.array_equal(cl_data.get(), np_data)
+
+		exit(0)
+
+	
+	sort.build()
+
+
+	cl_search_array = cl.array.to_device(queue, random_search_array)
+	cl_related_arrays = [cl.array.to_device(queue, x) for x in random_related_arrays]
+	sort(queue, cl_search_array.data, *[x.data for x in cl_related_arrays])
+
+
+	# check key buffer
+	cl_sorted = cl_search_array.get()
+	for system in range(systems):
+		assert np.array_equal(cl_sorted[system], np.sort(random_search_array[system]))
+
+	# check related buffers
+	index_of_sort = np.argsort(random_search_array)
+
+	sorted_related_arrays = []
+	for related_array in random_related_arrays:
+		sorted_related_array = np.zeros(systems*dim*dim).reshape(systems, dim, dim)
+		for system in range(systems):
+			sorted_related_array[system] = related_array[system][index_of_sort[system]]
+
+		sorted_related_arrays.append(sorted_related_array)
+
+
+
+
+	for cl_data, np_data in zip(cl_related_arrays, sorted_related_arrays):
+		#print(cl_data.get())
+		#print(np_data)
+		assert np.array_equal(cl_data.get(), np_data)
+
+
+
+
+
+
+
+
+
+
---
+++ cllib/common/kernel_helpers.py
@@ -0,0 +1,154 @@
+#-*- coding: utf-8 -*-
+"""
+helpers for kernel building objects.
+
+:author: Nicolas 'keksnicoh' Heimann 
+"""
+
+# XXX
+# - process_arguments_declaration: find possible numpy dtypes corresponding
+#   to floatn, intn, ... (e.g. dtype([np.float32, np.float32]))
+
+import pyopencl as cl 
+import pyopencl.tools
+
+import numpy as np 
+
+from copy import copy
+
+CL_TYPES = [
+    # TODO
+    # - abstract opencl types:
+    #    https://www.khronos.org/registry/cl/sdk/1.2/docs/man/xhtml/abstractDataTypes.html
+    # - keep reserved types in mind 
+    #    https://www.khronos.org/registry/cl/sdk/1.2/docs/man/xhtml/reservedDataTypes.html
+    'bool','char','uchar','short','ushort','int','uint', 'long', 'ulong', 'float', 'double', 'half', 
+    'size_t','ptrdiff_t','intptr_t','uintptr_t','void',
+    'half2','half3','half4','half8','half16',
+    'char2','char3','char4','char8','char16',
+    'uchar2','uchar3','uchar4','uchar8','uchar16',
+    'short2','short3','short4','short8','short16',
+    'ushort2','ushort3','ushort4','ushort8','ushort16',
+    'int2','int3','int4','int8','int16',
+    'uint2','uint3','uint4','uint8','uint16', 
+    'long2','long3','long4','long8','long16', 
+    'ulong2','ulong3','ulong4','ulong8','ulong16', 
+    'float2','float3','float4','float8','float16', 
+    'double2','double3','double4','double8','double16', 
+    'image2d_t','image3d_t','image2d_array_t','image1d_t','image1d_buffer_t','image1d_array_t','sampler_t','event_t',
+]
+
+
+def get_attribute_or_item(value, attr):
+    """
+    check whether value has attribute attr
+    or value[attr] is accessible and returns 
+    first match.
+    """
+    if hasattr(value, attr):
+        return getattr(value, attr)
+    elif (hasattr(value, '__contains__')
+        and attr in value 
+        and hasattr(value, '__getitem__')):
+        return map_expr[attr]
+
+def ensure_valid_cl_type(type):
+    if type not in CL_TYPES:
+        raise ValueError('invalid cl_type "{}"'.format(type))
+
+def process_arguments_declaration(device, arguments):
+    """
+    process tuple argument declarations.
+    find structures, check types and convert 
+    numpy dtypes to cl_types. 
+    """
+    strcts = [] 
+    arg_declr = [None]*len(arguments)
+    require_complex_numbers = False 
+
+    for i, cl_arg in enumerate(arguments):
+        cl_type = cl_arg[2]
+        if type(cl_type) is np.dtype and cl_type.fields is None:
+            cl_type = getattr(np, cl_type.name)
+
+        if type(cl_type) is str:
+            try:
+                ensure_valid_cl_type(cl_type)
+                arg_declr[i] = arguments[i]
+            except ValueError:
+                raise ValueError((
+                    'argument #{} "{}" invalid cl_type "{}" '
+                    'checkout https://www.khronos.org/registry/cl/sdk/1.2/docs/man/xhtml/dataTypes.html for available types.'
+                ).format(i, cl_arg[0], cl_type))
+
+        elif type(cl_type) is np.dtype:
+            strct_name = 't_{}'.format(cl_arg[0])
+            _, c_decl = cl.tools.match_dtype_to_c_struct(
+                device, 
+                strct_name, 
+                cl_type,
+            )
+            strcts.append(c_decl)
+            arg_declr[i] = (cl_arg[0], cl_arg[1], strct_name, cl_arg[3])
+
+        elif cl_type == np.int64: arg_declr[i] = (cl_arg[0], cl_arg[1], 'long', cl_arg[3])
+        elif cl_type == np.uint64: arg_declr[i] = (cl_arg[0], cl_arg[1], 'ulong', cl_arg[3])
+        elif cl_type == np.int32: arg_declr[i] = (cl_arg[0], cl_arg[1], 'int', cl_arg[3])
+        elif cl_type == np.uint32: arg_declr[i] = (cl_arg[0], cl_arg[1], 'uint', cl_arg[3])
+        elif cl_type == np.int16: arg_declr[i] = (cl_arg[0], cl_arg[1], 'short', cl_arg[3])
+        elif cl_type == np.uint16: arg_declr[i] = (cl_arg[0], cl_arg[1], 'ushort', cl_arg[3])
+        elif cl_type == np.int8: arg_declr[i] = (cl_arg[0], cl_arg[1], 'char', cl_arg[3])
+        elif cl_type == np.uint8: arg_declr[i] = (cl_arg[0], cl_arg[1], 'uchar', cl_arg[3])
+        elif cl_type == np.float16: arg_declr[i] = (cl_arg[0], cl_arg[1], 'half', cl_arg[3])
+        elif cl_type == np.float32: arg_declr[i] = (cl_arg[0], cl_arg[1], 'float', cl_arg[3])
+        elif cl_type == np.float64: raise NotImplementedError('double precision behavior not defined yet.')
+        elif cl_type == np.complex64: 
+            arg_declr[i] = (cl_arg[0], cl_arg[1], 'cfloat_t', cl_arg[3])
+            require_complex_numbers = True
+
+        elif cl_type == np.complex128: raise NotImplementedError('auto complex numbers behavior not defined yet.')
+        
+        else:
+            raise ValueError((
+                'argument #{} "{}" type "{}" not understood. '
+                'Must be string cl_type or numpy.dtype. '
+                'Supported numpty types are: np.dtype, np.(u)int(8|16|32|64), '
+                'np.float(16|32|64), np.complex(32|64|128)'
+            ).format(i, cl_arg[0], cl_arg[2]))
+
+    cl_args = [' '.join(a[1:]) for a in arg_declr]
+
+    libs = []
+    if require_complex_numbers:
+        libs.append('#include <pyopencl-complex.h>')
+
+    return arg_declr, strcts, cl_args, '\n'.join(libs)
+
+def create_knl_args_ordered(arg_declr, args, kwargs):
+    """
+    creates a list of kernel arguments from args and kwargs
+    by using arg_declr (maybe generated by process_arguments_declaration)
+    """
+    knl_args = []
+    available_args = copy(arg_declr)
+
+    for arg in args:
+        knl_args.append(arg)
+        del available_args[0]
+
+    for name in available_args:
+        if name not in kwargs:
+            raise ValueError('argument "{}" missing'.format(name))
+        knl_args.append(kwargs[name])
+        del kwargs[name]
+        
+    if len(kwargs):
+        for key in kwargs:
+            if key in arg_declr:
+                raise ArgumentError((
+                    'cannot use {} as keyword argument'
+                    + ' since it is allready used as '
+                    + 'positional argument').format(key)
+                )
+        raise ArgumentError('unkown argument: {}'.format(', '.join(kwargs.keys())))
+    return knl_args
